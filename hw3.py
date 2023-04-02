a7c8d1930ffd383aebfcec3f883142dda3f710b6
Valeriia Garkun <leragarkun41@gmail.com>
2023-04-02 15:27:46 +0300

    added hw3

---
+++ hw3.py
@@ -0,0 +1,5 @@
+countries = ["Ukraine", "Spain", "Italy"]
+capitals = ["Kyiv", "Madrid", "Rome"]
+print(countries[0] + ": " + capitals[0])
+print(countries[1] + ": " + capitals[1])
+print(countries[2] + ": " + capitals[2])
