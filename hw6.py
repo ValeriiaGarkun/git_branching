040b5899dcca43bd20bd3f0e030c6747a22d2dcf
Valeriia Garkun <leragarkun41@gmail.com>
2023-04-06 15:05:05 +0300

    add hw6

---
+++ hw6.py
@@ -0,0 +1,5 @@
+height = int(input("Enter height of rectangular: "))
+width = int(input("Enter width of rectangular: "))
+symbol = input("Enter symbol to build rectangular with: ")
+for i in range(height):
+    print(width * symbol)
